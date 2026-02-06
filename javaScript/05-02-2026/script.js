8da387df25f1a6b9a6a80fe795eb7deda74e109a
Rishabh Paudel <24BCS10110@cuchd.in>
2026-02-06 11:08:06 +0530

    modified bulb and now workin

--- javaScript/05-02-2026/script.js
+++ javaScript/05-02-2026/script.js
@@ -26,15 +26,31 @@ function riyalFun2() {
     document.getElementById("daRiyalText").innerText = "Coursor is outside div";
 }
 
-const bulbChalao = document.getElementById("bulbBtn");
-bulbChalao.addEventListener("click", bulbFun1);
-bulbChalao.addEventListener("click", bulbFun2);
-// bulbChalao.addEventListener("")
-function bulbFun1() {
-    document.getElementById("bulbImages").innerText = "./assets/onn.png"
-    document.getElementById("bulbBtn").innerHTML = "turn off";
-}
-function bulbFun2() {
-    document.getElementById("bulbImages").innerText = "./assets/off.png"
-    document.getElementById("bulbBtn").innerHTML = "turn onn";
-}
+// const bulbChalao = document.getElementById("bulbBtn");
+// bulbChalao.addEventListener("click", bulbFun1);
+// bulbChalao.addEventListener("click", bulbFun2);
+// // bulbChalao.addEventListener("")
+// function bulbFun1() {
+//     document.getElementById("bulbImages").innerText = "./assets/onn.png"
+//     document.getElementById("bulbBtn").innerHTML = "turn off";
+// }
+// function bulbFun2() {
+//     document.getElementById("bulbImages").innerText = "./assets/off.png"
+//     document.getElementById("bulbBtn").innerHTML = "turn onn";
+// }
+
+const bulbBtn = document.getElementById("bulbBtn");
+const bulbImage = document.getElementById("bulbImage");
+
+let isOn = false;
+
+bulbBtn.addEventListener("click", () => {
+    if (isOn) {
+        bulbImage.src = "./assets/off.png";
+        bulbBtn.innerHTML = "Turn On";
+    } else {
+        bulbImage.src = "./assets/onn.png";
+        bulbBtn.innerHTML = "Turn Off";
+    }
+    isOn = !isOn;
+});
