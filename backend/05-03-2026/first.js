e0ee07a697710565c275290f32d6ae1b02f8c7cc
Rishabh Paudel <24BCS10110@cuchd.in>
2026-03-05 10:41:23 +0530

    oficially started backed yayay noic

---
+++ backend/05-03-2026/first.js
@@ -0,0 +1,13 @@
+console.log(20+20);
+console.log("Hello");
+
+let x = 10;
+let y = 20;
+console.log(x + y);
+
+// ..............................
+
+function fruit(item) {
+    console.log(item);
+}
+fruit('apple');
---
+++ backend/05-03-2026/module.js
@@ -0,0 +1,20 @@
+// Core module
+
+// 1. fs module, imported usin require 
+const ab = require('fs');
+ab.writeFileSync("dummy.txt", "Learning Code");
+
+// 2. os module
+const xy = require('os');
+console.log(xy.platform());     // o/p = win32 even if its 64 it will be win32
+console.log(xy.hostname());     // user/client name
+console.log(xy.cpus());         // cpu information
+// 
+console.log(process.cwd());     // cwd: current workin directory, o/p: path
+console.log(process.pid);       // id of nodejs application
+
+// 3. colors
+const color = require('colors');
+console.log(color.red("Hello"));
+console.log(color.yellow("World"));
+console.log(color.black("good"));
