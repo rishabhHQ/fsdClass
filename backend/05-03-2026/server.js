21bc80dfff83ff8cadca4b6851b9957eb2659386
Rishabh Paudel <24BCS10110@cuchd.in>
2026-03-06 10:36:51 +0530

    did more changes in server .js

--- backend/05-03-2026/server.js
+++ backend/05-03-2026/server.js
@@ -1,7 +1,18 @@
 const http = require('http');
 http.createServer((req,resp) => {       // request and responce
 
+
+    console.log(req.url);
+    console.log(req.headers.host);      // root doamin name
+    console.log(req.method);
+    
+
+
+
+
     resp.write("<h1>Hello, this made from node js</h1>");    
+
+    
 
     // this is very important otherwise page will be in always reloading
     resp.end("Learning Node");          // very important
@@ -13,6 +24,8 @@ http.createServer((req,resp) => {       // request and responce
 
 const http2 = require('http');
 http2.createServer((req,resp) => {       
+
+    console.log(req.url);
 
     resp.setHeader("content-Type", "text/html");        // this will fix the error which was comin without this
     resp.write("<h2>Hello, this made from node js and this has different port now its nodemon</h2>");    
