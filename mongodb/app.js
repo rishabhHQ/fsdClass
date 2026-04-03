417029194278849b038efe0c6e06b25c79873849
Rishabh Paudel <24BCS10110@cuchd.in>
2026-04-03 10:44:18 +0530

    added mongo db app

--- mongodb/app.js
+++ mongodb/app.js
@@ -11,25 +11,29 @@ const Student = mongoose.model("students", studentSchema);  // parameters: (coll
 
 async function insertData() {
     try {
-        await Student.create({
+        await Student.create({      // .create of mongoose is .insertOne of mongo 
             name: "Rahul",
             age: 21,
             course: "B.Tech"
         });
-        console.log("Inserted one Student.");
+        console.log("Inserted one Student.");   // optinal but tells work is done
 
         await Student.insertMany([
             {name: "Aman", age: 19, course: "BCA"},
             {name: "Raman", age: 20, course: "B.Com"}
         ]);
-        console.log("Inserted many Students");
+        console.log("Inserted many Students");      // optinal but tells work is done
+        mongoose.connection.close();
     }
     catch (err) {
         console.err(err.message);
     }
 }
 
+// kind of main() function
 mongoose.connect(uri, {dbName: "schooldb"}).then(() => {
     console.log("connected to Atlas");
     insertData();
-}).catch(err => console.log(err));  
+}).catch(err => console.log(err));
+
+// .then and .catch are promises just like try catch
